01074bfcc0e8f0b5f53b182a81a22bba5293f63b
sangminkim <apfhdznzl@gmail.com>
2021-03-06 05:36:10 +0900

    #2 프로젝트의 규모가 커져 생기는 빌드 오류를 해결하기 위해 MultiDex 사용

---
+++ WatchaProject/app/src/main/java/com/sangmee/watchaproject/GlobalApplication.kt
@@ -0,0 +1,10 @@
+package com.sangmee.watchaproject
+
+import androidx.multidex.MultiDexApplication
+
+class GlobalApplication : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
