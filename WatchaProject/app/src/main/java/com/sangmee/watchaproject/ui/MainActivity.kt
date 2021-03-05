c365804bf00a2f96928b8f967d20191d4cdc0313
sangminkim <apfhdznzl@gmail.com>
2021-03-05 20:02:46 +0900

    #1 Create TrackFragment, FavoriteFragment &  MainActivity에 Fragment 부착

--- WatchaProject/app/src/main/java/com/sangmee/watchaproject/ui/MainActivity.kt
+++ WatchaProject/app/src/main/java/com/sangmee/watchaproject/ui/MainActivity.kt
@@ -3,11 +3,34 @@ package com.sangmee.watchaproject.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sangmee.watchaproject.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        initView()
+    }
+
+    private fun initView() {
+        //BottomNavigationView 세팅
+        nv_menu.post { nv_menu.selectedItemId = R.id.trackItem }
+
+        nv_menu.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.trackItem -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fl_container, TrackFragment()).commit()
+                    true
+                }
+                R.id.favoriteItem -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fl_container, FavoriteFragment()).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
