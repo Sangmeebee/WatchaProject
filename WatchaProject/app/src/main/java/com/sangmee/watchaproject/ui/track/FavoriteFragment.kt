3cc60ad6b03001d12dbe97b8cb0e9568fff38376
sangminkim <apfhdznzl@gmail.com>
2021-03-07 21:41:49 +0900

    #4 FavoriteFragment에 DB에서 불러온 즐겨찾기 목록 추가

--- WatchaProject/app/src/main/java/com/sangmee/watchaproject/ui/track/FavoriteFragment.kt
+++ WatchaProject/app/src/main/java/com/sangmee/watchaproject/ui/track/FavoriteFragment.kt
@@ -1,13 +1,23 @@
-package com.sangmee.watchaproject.ui
+package com.sangmee.watchaproject.ui.track
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 import com.sangmee.watchaproject.R
+import com.sangmee.watchaproject.model.Track
+import com.sangmee.watchaproject.ui.MainViewModel
+import kotlinx.android.synthetic.main.fragment_favorite.*
 
-class FavoriteFragment : Fragment() {
+class FavoriteFragment : Fragment(), TrackAdapter.OnClickListener {
+
+    private val trackAdapter = TrackAdapter()
+    private val vm by activityViewModels<MainViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +29,36 @@ class FavoriteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        initViewModel()
+    }
+
+
+    private fun initView() {
+        trackAdapter.onClickListener = this
+
+        rv_track.apply {
+            adapter = trackAdapter
+            setHasFixedSize(true)
+            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+        }
+    }
+
+    private fun initViewModel() {
+
+        vm.favorites.observe(viewLifecycleOwner, Observer {
+            trackAdapter.clearAndAddTracks(it)
+        })
+    }
+
+    override fun onClickToggleBtn(track: Track, isFavorite: Boolean) {
+        track.isFavorite = isFavorite
+        vm.updateFavoriteTrack(track)
+    }
+
+    override fun onDestroy() {
+        vm.unbindViewModel()
+        super.onDestroy()
     }
 }
